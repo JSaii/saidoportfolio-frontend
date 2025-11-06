57add5b193104392ffde92fcc64cef79f9bee655
JSaii <38206432+JSaii@users.noreply.github.com>
2025-11-06 21:58:03 +0900

    Finish about section, resume section, add side navigation bar on resume, fix layout and interactions

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 const tabs = document.querySelectorAll(".nav-btn");
 const sections = document.querySelectorAll(".tab-content");
 
+const savedTabId = localStorage.getItem("activeTabId") || "about-btn";
+const savedSectionId =
+    savedTabId === "about-btn" ? "about" :
+    savedTabId === "resume-btn" ? "resume" : "website";
+
+
+document.getElementById(savedTabId).classList.add("active");
+document.getElementById(savedSectionId).classList.add("active");
+document.getElementById(savedSectionId).style.display = "block";
+document.getElementById(savedSectionId).style.opacity = "1";
+
+
 tabs.forEach(tab => {
     tab.addEventListener("click", () => {
         if (tab.classList.contains("active")) {
@@ -21,7 +33,7 @@ tabs.forEach(tab => {
 
         const targetId =
             tab.id === "about-btn" ? "about" :
-            tab.id === "resume-btn" ? "resume" : "website";
+                tab.id === "resume-btn" ? "resume" : "website";
         const next = document.getElementById(targetId);
 
         next.style.display = "block";
@@ -31,17 +43,19 @@ tabs.forEach(tab => {
 
         window.scrollTo({ top: 0, behavior: "smooth" });
 
+        localStorage.setItem("activeTabId", tab.id);
+
     });
 });
 
 function adjustMainHeight() {
-  const header = document.querySelector(".header");
-  const main = document.querySelector("main");
+    const header = document.querySelector(".header");
+    const main = document.querySelector("main");
 
-  if (header && main) {
-    const headerHeight = header.offsetHeight;
-    main.style.maxHeight = `calc(100vh - ${headerHeight}px)`;
-  }
+    if (header && main) {
+        const headerHeight = header.offsetHeight;
+        main.style.maxHeight = `calc(100vh - ${headerHeight}px)`;
+    }
 }
 
 window.addEventListener("load", adjustMainHeight);
